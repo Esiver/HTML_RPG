ac0b907d61d63d26a9b781286c61429a1c0d83cc
EmilNS <emilarme@gmail.com>
2021-04-04 15:50:19 +0800

    targetting

--- rules.js
+++ rules.js
@@ -4,6 +4,7 @@ var FOE_NUM = 1
 
 const tileContainer = document.querySelector(".tileContainer");
 
+// for x axis
 for (w = 0; w < MAP_WIDTH; w++) {
   var row = document.createElement("li");
   var rowId = "row-" + w;
@@ -12,40 +13,52 @@ for (w = 0; w < MAP_WIDTH; w++) {
 
   tileContainer.append(row);
 
-  for (h = 0; h < MAP_HEIGHT; h++) {
-    var col = document.createElement("li");
-    var colId = "row-" + w + "_col-" + h;
-    col.className = "tile";
+    // for y axis
+    for (h = 0; h < MAP_HEIGHT; h++) {
+        var col = document.createElement("li");
+        var colId = "row-" + w + "_col-" + h;
+        col.className = "tile";
 
-    col.setAttribute("id", colId);
-    row.append(col);
+        col.setAttribute("id", colId);
+        row.append(col);
   }
 }
 
-// player object
-function Unit(pos_x, pos_y) {
+// unit objects
+function Unit(pos_x, pos_y, name) {
   this.pos_x = pos_x;
   this.pos_y = pos_y;
+  this.name = name;
 }
-var player_one = new Unit(1,1);
 
+//initialize players 👦 
+var player_one = new Unit(1,1, 'player');
 
 var pPos = document.createElement('div');
+pPos.innerHTML = player_one.name;
 pPos.setAttribute('id', 'player');
 
 // initialize player by appending a div
 document.querySelector('#row-'+player_one.pos_x+'_col-'+player_one.pos_y).appendChild(pPos);
 
+//monster
+var monster = document.createElement('p');
+monster.className = 'monster'
+document.querySelector('#row-5_col-2').appendChild(monster);
+
+var moo = document.createElement('p');
+moo.className = 'monster'
+//document.querySelector('#row-5_col-2').appendChild(moo);
+
 // key presses & control
 function move (x, y) {
-  // need logic for blocking...
-  //
-  player_one.pos_x = player_one.pos_x + x;
+  player_one.pos_x = player_one.pos_x + x; //increment coordinates
   player_one.pos_y = player_one.pos_y + y;
   document.querySelector('#row-'+player_one.pos_x+'_col-'+player_one.pos_y).appendChild(pPos);
   
-  // do position check
-  check();
+  check(); // do position check everytime we move
+
+    // Todo: need logic for blocking...
 }
 
 document.addEventListener('keydown', logKey);
@@ -66,19 +79,19 @@ function logKey(e) {
 
 // position check function
 function check() {
-  let p = document.querySelector('#row-'+player_one.pos_x+'_col-'+player_one.pos_y);
-  //console.log(p);
-  console.log(p.children)
-  let area = p.children;
-  Array.from(area).forEach(p => {
-    console.log(area);
-});
-  if (p.contains(monster)) {
-    
+  let playerPos = document.querySelector('#row-'+player_one.pos_x+'_col-'+player_one.pos_y);
+  
+  let ms = playerPos.getElementsByClassName('monster');
+  console.table(ms);
+  
+  
+  if (playerPos.contains(monster)) {
+      monsterEncounter()
   }
 }
 
-var monster = document.createElement('p');
-monster.innerHtml = 'M';
-monster.className = 'monster'
-document.querySelector('#row-5_col-2').appendChild(monster);
+
+// encounters
+function monsterEncounter() {
+    console.log('rawr')
+}
